a17b09f1d90486a619bc0693911888d1012fcf91
David Robles <drobles@gmail.com>
2016-12-10 17:33:11 -0500

    Move zs-tmux to ~/projects

--- zs-tmux.sh
+++ zs-tmux.sh
@@ -6,7 +6,7 @@ tmux has-session -t $SESSIONNAME &> /dev/null
 if [ $? != 0 ]
  then
     tmux new-session -s $SESSIONNAME -n script -d
-    tmux send-keys -t $SESSIONNAME "~/zs-tmux/zs-tmux-script.sh" C-m
+    tmux send-keys -t $SESSIONNAME "~/projects/zs-tmux/zs-tmux-script.sh" C-m
 fi
 
 tmux attach -t $SESSIONNAME
